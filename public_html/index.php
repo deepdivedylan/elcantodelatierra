fd6849bcd33ff3d1ca1436be39924a9ab448955e
Dylan McDonald <dmcdonald21@cnm.edu>
2018-02-01 11:14:36 -0700

    resistance is futile

--- public_html/index.php
+++ public_html/index.php
@@ -5,12 +5,23 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
+		<link rel="stylesheet" href="css/elcantodelatierra.css" />
 
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
 		<title>El Canto de la Tierra</title>
 	</head>
-	<body>
+	<body class="sticky-footer">
+		<div class="sticky-footer-content">
+			<h1>El Canto</h1>
+			<blockquote>
+				<p>Resistance is futile.</p>
+				<footer>&mdash;The Borg</footer>
+			</blockquote>
+		</div>
+		<footer>
+			This is a footer.
+		</footer>
 	</body>
 </html>
