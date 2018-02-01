d4fa280bb074239c6624dddd2e9b4eb09ee46381
Dylan McDonald <dmcdonald21@cnm.edu>
2018-02-01 12:03:35 -0700

    added language to navbar

--- public_html/index.php
+++ public_html/index.php
@@ -4,8 +4,8 @@
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css" />
-		<link rel="stylesheet" href="css/elcantodelatierra.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/css/bootstrap.min.css"/>
+		<link rel="stylesheet" href="css/elcantodelatierra.css"/>
 
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
@@ -14,11 +14,40 @@
 	</head>
 	<body class="sticky-footer">
 		<div class="sticky-footer-content">
-			<h1>El Canto</h1>
-			<blockquote>
-				<p>Resistance is futile.</p>
-				<footer>&mdash;The Borg</footer>
-			</blockquote>
+			<header>
+				<nav class="navbar navbar-expand-md navbar-light bg-light">
+					<a class="navbar-brand" href="/"><img src="https://placekitten.com/g/32/32" alt="" /> El Canto</a>
+					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+						<span class="navbar-toggler-icon"></span>
+					</button>
+
+					<div class="collapse navbar-collapse" id="navbarSupportedContent">
+						<ul class="navbar-nav mr-auto">
+							<li class="nav-item">
+								<a class="nav-link" href="/">Home</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/assimilation/">Assimilation</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="/fuzzy/">Fuzzy</a>
+							</li>
+						</ul>
+						<ul class="navbar-nav navbar-right">
+							<li class="nav-item">
+								<a href="/en/"><img class="flagIcon" src="/images/flag-us.svg" alt="" /> English</a>
+							</li>
+						</ul>
+					</div>
+				</nav>
+			</header>
+			<main class="container">
+				<h1>El Canto</h1>
+				<blockquote>
+					<p>Resistance is futile.</p>
+					<footer>&mdash;The Borg</footer>
+				</blockquote>
+			</main>
 		</div>
 		<footer class="sticky-footer-footer">
 			This is a footer.
