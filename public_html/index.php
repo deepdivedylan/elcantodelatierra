656c406dead19f2a549ad960f53227ecd3e72535
Dylan McDonald <dmcdonald21@cnm.edu>
2018-02-03 11:11:13 -0700

    added font awesome and mended the nav

--- public_html/index.php
+++ public_html/index.php
@@ -10,6 +10,7 @@
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
 		<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+		<script type="text/javascript" src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 		<title>El Canto de la Tierra</title>
 	</head>
 	<body class="sticky-footer">
@@ -35,7 +36,7 @@
 						</ul>
 						<ul class="navbar-nav navbar-right">
 							<li class="nav-item">
-								<a href="/en/"><img class="flagIcon" src="/images/flag-us.svg" alt="" /> English</a>
+								<a class="nav-link" href="/en/"><img class="flagIcon" src="/images/flag-us.svg" alt="" /> English</a>
 							</li>
 						</ul>
 					</div>
